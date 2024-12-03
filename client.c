c0b944b710db4c7269994224e68ccba76276fcf5
juhanse <julien.hanse12@gmail.com>
2024-12-03 23:36:19 +0100

    add: main function in client

--- client.c
+++ client.c
@@ -6,8 +6,25 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 23:30:23 by juhanse           #+#    #+#             */
-/*   Updated: 2024/12/03 23:30:28 by juhanse          ###   ########.fr       */
+/*   Updated: 2024/12/03 23:36:06 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+int	main(int argc, char **argv)
+{
+	int		pid;
+	char	*message;
+
+	if (argc != 3)
+	{
+		printf("Error: Entrez un PID et un Message");
+		return (0);
+	}
+	pid = atoi(argv[1]);
+	message = argv[2];
+	printf("PID: %d\n", pid);
+	printf("Message: %s", message);
+	return (0);
+}
