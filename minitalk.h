7af63c32afd375e64c478b25fd0e17b4eb954600
juhanse <julien.hanse12@gmail.com>
2024-12-03 23:33:19 +0100

    add: stdio and string import

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 23:29:45 by juhanse           #+#    #+#             */
-/*   Updated: 2024/12/03 23:30:18 by juhanse          ###   ########.fr       */
+/*   Updated: 2024/12/03 23:33:06 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@
 # include <signal.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
 
 #endif
