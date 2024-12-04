0347350319adc67d9975412f02aef3c1650f1d00
Julien Hanse <julien.hanse12@gmail.com>
2024-12-04 14:16:46 +0100

    add: signal_handler server function

--- server.c
+++ server.c
@@ -6,14 +6,25 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 23:30:31 by juhanse           #+#    #+#             */
-/*   Updated: 2024/12/03 23:33:56 by juhanse          ###   ########.fr       */
+/*   Updated: 2024/12/04 14:16:12 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	signal_handler(int signum)
+{
+	
+}
+
 int	main(void)
 {
+	struct sigaction	action;
+
 	printf("%d", getpid());
+	while (1)
+	{
+		pause();
+	}
 	return (0);
 }
