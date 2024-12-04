64703fb41853e58f59774d6562b032a881125e5d
Julien Hanse <julien.hanse12@gmail.com>
2024-12-04 14:32:09 +0100

    add: vars in signal_handler

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 23:30:31 by juhanse           #+#    #+#             */
-/*   Updated: 2024/12/04 14:16:12 by juhanse          ###   ########.fr       */
+/*   Updated: 2024/12/04 14:25:35 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,13 @@
 
 void	signal_handler(int signum)
 {
-	
+	static int	bits;
+	static int	char_result;
+	static int	len;
+	static char	*result;
+
+	if (!result)
+		result = ft_strdup("");
 }
 
 int	main(void)
@@ -22,6 +28,11 @@ int	main(void)
 	struct sigaction	action;
 
 	printf("%d", getpid());
+	action.sa_handler = signal_handler;
+	action.sa_flags = 0;
+	sigemptyset(&action.sa_mask);
+	sigaction(SIGUSR1, &action, NULL);
+	sigaction(SIGUSR2, &action, NULL);
 	while (1)
 	{
 		pause();
