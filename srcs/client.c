b9fab45bc6cb922e62bd3643c36fe7fd0b759195
juhanse <julien.hanse12@gmail.com>
2025-01-07 12:23:27 +0100

    refactor: cleaning code

--- srcs/client.c
+++ srcs/client.c
@@ -6,44 +6,33 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 23:30:23 by juhanse           #+#    #+#             */
-/*   Updated: 2025/01/06 00:41:32 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/01/07 12:22:59 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
 
-void	ft_error(int type, int pid)
+void	send(int pid, char *msg)
 {
-	if (type == 1)
-		ft_printf("Usage: ./client [Server PID] [String to send]\n");
-	else if (type == 2 && pid != -1)
-		ft_printf("ERROR: Invalid PID: %d\n", pid);
-	else if (type == 3 && pid != -1)
-		ft_printf("ERROR: Failed to send signal to PID: %d\n", pid);
-	exit(EXIT_FAILURE);
-}
-
-void	send_message(int pid, char *msg)
-{
-	int	i;
+	int	bit;
 	int	letter;
 
 	letter = 0;
 	while (msg[letter])
 	{
-		i = -1;
-		while (++i < 8)
+		bit = -1;
+		while (++bit < 8)
 		{
-			if (((unsigned char)(msg[letter] >> (7 - i)) & 1) == 0)
+			if (((unsigned char)(msg[letter] >> (7 - bit)) & 1) == 0)
 				kill(pid, SIGUSR1);
-			else if (((unsigned char)(msg[letter] >> (7 - i)) & 1) == 1)
+			else if (((unsigned char)(msg[letter] >> (7 - bit)) & 1) == 1)
 				kill(pid, SIGUSR2);
 			usleep(50);
 		}
 		letter++;
 	}
-	i = 0;
-	while (i++ < 8)
+	bit = 0;
+	while (bit++ < 8)
 	{
 		kill(pid, SIGUSR1);
 		usleep(50);
@@ -55,12 +44,16 @@ int	main(int argc, char **argv)
 	int	pid;
 
 	if (argc != 3)
-		ft_error(1, -1);
+	{
+		ft_printf("Usage: ./client [Server PID] [String to send]\n");
+		return (0);
+	}
 	pid = ft_atoi(argv[1]);
-	if (pid <= 0 || pid > 99998)
-		ft_error(2, pid);
 	if (kill(pid, 0) < 0)
-		ft_error(3, pid);
-	send_message(pid, argv[2]);
-	exit(EXIT_SUCCESS);
+	{
+		ft_printf("ERROR: Failed to send signal to PID: %d\n", pid);
+		return (0);
+	}
+	send(pid, argv[2]);
+	return (0);
 }
--- srcs/server.c
+++ srcs/server.c
@@ -6,53 +6,37 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 23:30:31 by juhanse           #+#    #+#             */
-/*   Updated: 2025/01/06 01:35:17 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/01/07 12:18:48 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
 
-void	sigint_handler(int sig_num)
+void	receive(int signum)
 {
-	(void)sig_num;
-	ft_printf("Received SIGINT signal, exiting...\n");
-	exit(EXIT_SUCCESS);
-}
+	static int				i = 0;
+	static unsigned char	c = 0;
 
-void	handle_signal(int signum, siginfo_t *info, void *context)
-{
-	int			client_pid;
-	static int	bit_count = 0;
-	static char	current_char = 0;
-
-	(void)context;
-	client_pid = info->si_pid;
-	if (signum == SIGUSR1)
-		current_char |= (1 << bit_count);
-	bit_count++;
-	if (bit_count == 8)
+	if (signum == SIGUSR2)
+		c = c << 1;
+	else if (signum == SIGUSR1)
+		c = (c << 1) | 0b00000001;
+	i++;
+	if (i == 8)
 	{
-		write(1, &current_char, 1);
-		bit_count = 0;
-		current_char = 0;
+		ft_printf("%c", c);
+		i = 0;
+		c = 0;
 	}
-	kill(client_pid, SIGUSR1);
 }
 
 int	main(void)
 {
-	struct sigaction	action;
-
-	ft_printf("Server PID: %d\n", getpid());
-	action.sa_flags = SA_SIGINFO;
-	action.sa_sigaction = handle_signal;
-	sigemptyset(&action.sa_mask);
-	sigaction(SIGUSR1, &action, NULL);
-	sigaction(SIGUSR2, &action, NULL);
-	signal(SIGINT, sigint_handler);
+	ft_printf("PID: %d\n", getpid());
 	while (1)
 	{
-		pause();
+		signal(SIGUSR1, receive);
+		signal(SIGUSR2, receive);
 	}
 	return (0);
 }
