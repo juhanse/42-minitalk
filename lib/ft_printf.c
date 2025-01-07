9cd7ecf312612574ccbf74309a093a178e788eff
juhanse <julien.hanse12@gmail.com>
2025-01-07 17:29:54 +0100

    add: putstr in printf

--- lib/ft_printf.c
+++ lib/ft_printf.c
@@ -6,33 +6,43 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 11:57:38 by juhanse           #+#    #+#             */
-/*   Updated: 2025/01/07 11:58:48 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/01/07 16:57:15 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
 
-int	ft_print_char(char c, int count)
+int	ft_putchar(char c, int count)
 {
 	write(1, &c, 1);
 	count++;
 	return (count);
 }
 
-int	ft_print_nb(int nb, int count)
+int	ft_putstr(char *s, int count)
+{
+	int	i;
+
+	i = -1;
+	while (s[++i])
+		count = ft_putchar(s[i], count);
+	return (count);
+}
+
+int	ft_putnbr(int nb, int count)
 {
 	if (nb < 0)
 	{
-		count = ft_print_char('-', count);
+		count = ft_putchar('-', count);
 		nb = -nb;
 	}
 	if (nb > 9)
 	{
-		count = ft_print_nb(nb / 10, count);
-		count = ft_print_nb(nb % 10, count);
+		count = ft_putnbr(nb / 10, count);
+		count = ft_putnbr(nb % 10, count);
 	}
 	else
-		count = ft_print_char(nb + 48, count);
+		count = ft_putchar(nb + 48, count);
 	return (count);
 }
 
@@ -45,17 +55,20 @@ int	ft_printf(const char *s, ...)
 	i = -1;
 	count = 0;
 	va_start(args, s);
-	if (!s)
-		return (0);
 	while (s[++i])
 	{
-		if (s[i] == '%' && s[i + 1] == 'd')
+		if (s[i] == '%' && s[i + 1])
 		{
 			i++;
-			count = ft_print_nb(va_arg(args, int), count);
+			if (s[i] == 's')
+				count = ft_putstr(va_arg(args, char *), count);
+			else if (s[i] == 'd')
+				count = ft_putnbr(va_arg(args, int), count);
+			else
+				count = ft_putchar(s[i], count);
 		}
 		else
-			count = ft_print_char(s[i], count);
+			count = ft_putchar(s[i], count);
 	}
 	va_end(args);
 	return (count);
